17b4ed5f7687f03d91b6c9f05296f8485720aa7f
loosebooster <loosebooster@gmail.com>
2023-01-04 01:11:02 +0900

    commit

---
+++ 01_IntroCS/agree.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char question = get_char("Do you agree? (y/n)");
+
+    if (question == 'Y' || question == 'y')
+    {
+        printf("AGREED\n");
+    }
+    else if (question == 'N' || question == 'n')
+    {
+        printf("NOT AGREED\n");
+    }
+}
