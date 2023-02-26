4c574ad5329a29c4e501606dc6079d9744338893
loosebooster <loosebooster@gmail.com>
2023-02-26 12:49:02 +0900

    Alllll Right

---
+++ 01_IntroCS/compare.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = malloc(strlen(s) + 1);
+
+    strcpy(t, s);
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
