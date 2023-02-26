9705604af608763c134aa62719eec79a88770c3b
loosebooster <loosebooster@gmail.com>
2023-02-26 18:29:33 +0900

    Alllll Right

--- 01_IntroCS/scanf.c
+++ 01_IntroCS/scanf.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+/*
 int main(void)
 {
     int x;
     printf("x: ");
     scanf("%i", &x);
     printf("x: %i\n", x);
+}
+*/
+
+int main(void)
+{
+    char s[5];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 }
